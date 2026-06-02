adac4ff8ffbfa8d08017328ef57ed73ba776b9a4
zqzqhesam10-netizen <zqzq.hesam10@gmail.com>
2026-06-02 06:49:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-set -o errexit
-
-# تثبيت المكتبات
-pip install -r requirements.txt
-
-# إجبار Playwright على التثبيت في مسار ثابت يمكننا الوصول إليه
 export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright
+pip install -r requirements.txt
 playwright install chromium
