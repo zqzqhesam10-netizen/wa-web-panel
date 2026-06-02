926a30b459548829c81222aaa902a2a87ce30b3e
zqzqhesam10-netizen <zqzq.hesam10@gmail.com>
2026-06-02 06:20:57 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
 
+#!/usr/bin/env bash
+set -o errexit
+
+# تثبيت المكتبات
+pip install -r requirements.txt
+
+# تحميل المتصفح وتثبيت المكتبات المعتمدة للنظام (مهم جداً للينكس على Render)
+playwright install chromium
+playwright install-deps
